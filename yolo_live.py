741b8a0f99bf3f73adbf122ee84f019267616764
chescaallyza <duatinchesca@gmail.com>
2024-12-01 12:44:54 +0800

    Update yolo_live.py

--- yolo_live.py
+++ yolo_live.py
@@ -7,7 +7,7 @@ from draw_utils_live import plot_boxes_live, color_map_live
 # Function to handle live webcam detection
 def live_detection(plot_boxes, model_path="best.pt", webcam_resolution=(1280, 720)):
     frame_width, frame_height = webcam_resolution
-    cap = cv2.VideoCapture(1)
+    cap = cv2.VideoCapture(0)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
     model = YOLO(model_path).to('cpu')
